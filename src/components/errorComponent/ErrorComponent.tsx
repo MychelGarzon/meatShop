d2623ad5cd81e0412c8c3d1f6c367bc9d4394dad
mtjomy17@gmail.com <mtjomy17@gmail.com>
2024-05-27 14:16:08 +0300

    basic settings

---
+++ src/components/errorComponent/ErrorComponent.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useRouteError } from 'react-router-dom';
+
+interface RouteError {
+  status?: number;
+  statusText?: string;
+  message?: string;
+}
+
+const Error: React.FC = () => {
+  const error = useRouteError() as RouteError;
+  return (
+    <div>
+      <h1>Error</h1>
+      {error.status ? (
+        <>
+          <p>Status: {error.status}</p>
+          <p>Status Text: {error.statusText}</p>
+        </>
+      ) : (
+        <p>{error.message || 'An unexpected error occurred'}</p>
+      )}
+    </div>
+  );
+};
+
+export default Error;
