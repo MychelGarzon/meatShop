e04bfe9913985247e7c0c5b647ae6a12571f0e47
Atte Kivimaki <aj.kivimaki@gmail.com>
2024-05-28 12:58:53 +0300

    KAN-15 Cart container, no items view

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { Button } from '@mui/material';
+import styles from './Cart.module.css'
+import { Link } from 'react-router-dom';
+import data from '../../data/data';
 
-const cart = () => {
+const Cart: React.FC = () => {
+  const cartItems = [];
+
+  console.log(data)
+
   return (
-    <div>cart</div>
+    <>
+      {cartItems.length === 0
+        ?
+        <div className={styles['no-items']}>
+          <p>Aún no tienes productos seleccionados</p>
+          <Link to="/">
+            <Button variant='contained' color='primary' fullWidth >Regresar a productos</Button>
+          </Link>
+        </div>
+        :
+        <div>
+          <div className={styles.cart}>
+            <p>Producto</p>
+            <p>Cantidad</p>
+            <p>Precio por kilo</p>
+          </div>
+        </div>}
+    </>
   )
 }
 
-export default cart
+export default Cart
