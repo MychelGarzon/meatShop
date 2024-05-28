4f55d96952f3bd9524677fa5b8e1f876187d2473
Pinja Alanne <pinja.alanne@gmail.com>
2024-05-28 15:57:26 +0300

    KAN-22 Add basic card structure

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -1,6 +1,45 @@
-const ProductCard = () => {
+import { Button, Card, CardActions, CardContent, CardMedia, ThemeProvider, Typography, createTheme } from "@mui/material"
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import data from '../../data/data'
+import styles from './productCard.module.css'
+
+const ProductCard: React.FC = () => {
+
   return (
-    <div>ProductCard</div>
+    <Card
+      className={styles.productCard}
+      sx={{ maxWidth: 368 }}
+    >
+      <CardMedia
+        sx={{ height: 368, width: 368 }}
+        image={data[0].image}
+        title="product image"
+      />
+      <CardContent className={styles.cardContent}>
+        <Typography variant="body1" component="div">
+          Res
+        </Typography>
+        <Typography
+          variant="h5"
+          className={styles.productName}
+        >
+          {data[0].name}
+        </Typography>
+        <Typography variant="h6">
+          ${data[0].price}
+        </Typography>
+        <Typography variant="body1">
+          Precio por kilo
+        </Typography>
+        <Button
+          variant="outlined"
+          color="primary"
+          size="large"
+          className={styles.addToCardButton}>Agregar
+          <ShoppingCartIcon />
+        </Button>
+      </CardContent>
+    </Card >
   )
 }
 
