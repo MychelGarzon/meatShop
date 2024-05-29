ad79b906e5b1b45c26e5a107fc4a3db6c8e1adcd
Atte Kivimaki <aj.kivimaki@gmail.com>
2024-05-29 12:01:49 +0300

    remove styling from +/- btns

--- src/components/minusPlusButton/PlusButton.tsx
+++ src/components/minusPlusButton/PlusButton.tsx
@@ -1,24 +1,20 @@
-import Box from "@mui/material/Box";
 import SpeedDial from "@mui/material/SpeedDial";
 import SpeedDialIcon from "@mui/material/SpeedDialIcon";
 
 function PlusButton() {
   return (
-    <Box sx={{ height: 320, transform: "translateZ(0px)", flexGrow: 1 }}>
-      <SpeedDial
-        ariaLabel="SpeedDial basic example"
-        sx={{ position: "absolute"}}
-        icon={<SpeedDialIcon sx={{ color: "text.secondary" }} />}
-        FabProps={{
-          sx: {
+    <SpeedDial
+      ariaLabel="SpeedDial basic example"
+      icon={<SpeedDialIcon sx={{ color: "text.secondary" }} />}
+      FabProps={{
+        sx: {
+          bgcolor: "inherit",
+          "&:hover": {
             bgcolor: "inherit",
-            "&:hover": {
-              bgcolor: "inherit",
-            },
           },
-        }}
-      ></SpeedDial>
-    </Box>
+        },
+      }}
+    ></SpeedDial>
   );
 }
 
