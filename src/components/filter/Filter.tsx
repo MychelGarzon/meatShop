9d8755287ed86ffb3e643ac05779b084ae45ff60
Pinja Alanne <pinja.alanne@gmail.com>
2024-05-29 12:27:09 +0300

    KAN-28 add filter title

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -1,6 +1,16 @@
+import { Typography, Box } from '@mui/material'
+import styles from './filter.module.css'
+
 const Filter = () => {
   return (
-    <div>Filter</div>
+    <Box className={styles.filterBox}>
+      <Typography
+        variant="h6"
+        className={styles.filterTitle}
+      >
+        Selecciona la categoría de tu interés
+      </Typography>
+    </Box>
   )
 }
 
