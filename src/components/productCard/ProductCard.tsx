ad050150479a4e056e663d91d3f8d950774066d4
Pinja Alanne <pinja.alanne@gmail.com>
2024-05-29 11:24:02 +0300

    KAN-26-ProductCard add different card sizes

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -1,42 +1,56 @@
-import { Button, Card, CardContent, CardMedia, Typography } from "@mui/material"
+import { Button, Card, CardContent, CardMedia, Typography, useMediaQuery } from "@mui/material"
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import styles from './productCard.module.css'
 import { Products } from "../../data/data";
 
-const ProductCard: React.FC<Products> = ({ ...product
-}) => {
+const ProductCard: React.FC<Products> = ({ image, name, price, type }) => {
+  const XS = useMediaQuery('(min-width:0px)');
+  const S = useMediaQuery('(min-width:430px)');
+  const M = useMediaQuery('(min-width:834px)');
+  const L = useMediaQuery('(min-width:1024px)');
+
+  const buttonSize = XS ? 'small' : S ? 'medium' : M ? 'large' : L ? 'large' : 'large';
+
   return (
     <Card
       className={styles.productCard}
-      sx={{ maxWidth: 368 }}
     >
       <CardMedia
-        sx={{ height: 368, width: 368 }}
-        image={product?.image}
+        className={styles.cardMedia}
+        image={image}
         title="product image"
       />
       <CardContent className={styles.cardContent}>
-        <Typography variant="body1" component="div">
-          Res
+        <Typography
+          variant="body1"
+          component="div"
+          className={styles.productType}
+        >
+          {type}
         </Typography>
         <Typography
           variant="h5"
           className={styles.productName}
         >
-          {product?.name}
+          {name}
         </Typography>
-        <Typography variant="h6">
-          ${product?.price}
+        <Typography
+          variant="h6"
+          className={styles.productPrice}
+        >
+          ${price}
         </Typography>
-        <Typography variant="body1">
+        <Typography
+          variant="body1"
+          className={styles.pricePerKilo}>
           Precio por kilo
         </Typography>
         <Button
           variant="outlined"
           color="primary"
-          size="large"
+          size={buttonSize}
           className={styles.addToCardButton}>Agregar
-          <ShoppingCartIcon />
+          <ShoppingCartIcon className={styles.addToCartIcon} />
         </Button>
       </CardContent>
     </Card >
