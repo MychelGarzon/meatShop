3c46affb3b25eab547964bc0f89f31055d9de2d6
Atte Kivimaki <aj.kivimaki@gmail.com>
2024-05-30 13:43:59 +0300

    KAN-16 create formData

--- src/components/cartForm/CartForm.tsx
+++ src/components/cartForm/CartForm.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+import { Button, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField } from '@mui/material';
 import styles from './CartForm.module.css';
 
 interface FormData {
@@ -8,14 +10,50 @@ interface FormData {
   locality: string;
   phone: string;
   email: string;
-  comment?: string;
+  comments?: string;
 }
 
 const CartForm: React.FC = () => {
   const cities = ['Barrios Unidos', 'Chapinero', 'Suba', 'Teusaquillo', 'Usaquen'];
-  const formData: FormData[] = [];
+  const [isFormCompleted, setIsFormCompleted] = useState<boolean>(false);
+  const [formData, setFormData] = useState<FormData>({
+    name: '',
+    city: '',
+    address: '',
+    neighborhood: '',
+    locality: '',
+    phone: '',
+    email: '',
+    comments: '',
+  });
 
-  console.log(cities, formData)
+  const validateForm = (data: FormData) => {
+    return data.name !== '' &&
+      data.city !== '' &&
+      data.address !== '' &&
+      data.neighborhood !== '' &&
+      data.locality !== '' &&
+      data.phone !== '' &&
+      data.email !== '';
+  };
+
+  useEffect(() => {
+    setIsFormCompleted(validateForm(formData));
+  }, [formData]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | SelectChangeEvent<string>) => {
+    const target = event.target as HTMLInputElement;
+    const { name, value } = target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('Form Data:', formData);
+  };
 
   return <div className={styles['cart-form']}>
     <div className={styles['total-container']}>
@@ -43,37 +81,103 @@ const CartForm: React.FC = () => {
     <p className={styles.title}>Tus datos para la orden</p>
     <small className={styles.light}>* Campo obligatorio</small>
 
-    <form className={styles.form}>
-      <label>
-        <input type="text" required placeholder='Nombre *' name='name' />
-      </label>
-      <label>
-        <input type="text" required placeholder='Ciudad *' name='city' />
-      </label>
-      <label>
-        <input type="text" required placeholder='Dirección *' name='address' />
-      </label>
-      <label>
-        <input type="text" required placeholder='Barrio *' name='neighborhood' />
-      </label>
-      <label>
-        <input type="text" required placeholder='Localidad *' name='locality' />
-      </label>
-      <label>
-        <input type="text" required placeholder='Teléfono de contacto *' name='phone' />
-      </label>
-      <label>
-        <input type="text" required placeholder='Correo electrónico *' name='email' />
-      </label>
-      <label>
-        <input type="text" placeholder='Comentarios' name='comment' />
-      </label>
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <TextField
+        type='text'
+        id="outlined-basic"
+        label="Nombre"
+        name="name"
+        value={formData?.name}
+        variant="outlined"
+        fullWidth
+        onChange={handleChange}
+        required />
+      <FormControl fullWidth>
+        <InputLabel id="city" required>Ciudad</InputLabel>
+        <Select
+          labelId="city"
+          name='city'
+          value={formData?.city}
+          onChange={handleChange}
+        >
+          {cities.map((city) => (
+            <MenuItem key={city} value={city}>{city}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <TextField
+        type='text'
+        id="outlined-basic"
+        label="Dirección"
+        name="address"
+        value={formData?.address}
+        variant="outlined"
+        fullWidth
+        onChange={handleChange}
+        required />
+      <TextField
+        type='text'
+        id="outlined-basic"
+        label="Barrio"
+        name="neighborhood"
+        value={formData?.neighborhood}
+        variant="outlined"
+        fullWidth
+        onChange={handleChange}
+        required />
+      <TextField
+        type='text'
+        id="outlined-basic"
+        label="Localidad"
+        name="locality"
+        value={formData?.locality}
+        variant="outlined"
+        fullWidth
+        onChange={handleChange}
+        required />
+      <TextField
+        type='tel'
+        id="outlined-basic"
+        label="Teléfono de contacto"
+        name="phone"
+        value={formData?.phone}
+        variant="outlined"
+        fullWidth
+        onChange={handleChange}
+        required />
+      <TextField
+        type='email'
+        id="outlined-basic"
+        label="Correo electrónico"
+        name="email"
+        value={formData?.email}
+        variant="outlined"
+        fullWidth
+        onChange={handleChange}
+        required />
+      <TextField
+        type='text'
+        id="outlined-basic"
+        label="Comentarios"
+        name="comments"
+        value={formData?.comments}
+        variant="outlined"
+        fullWidth
+        onChange={handleChange} />
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        disabled={!isFormCompleted}
+        fullWidth>
+        Enviar orden
+      </Button>
     </form>
 
     <div className={styles['info-text']}>
       <p className={styles.light}>Luego de enviar tu orden nuestro equipo de logística se comunicará contigo para definir la fecha de envío de los productos y el método de pago.</p>
     </div>
-  </div>
+  </div >
 }
 
 export default CartForm;
