e50d177c2c59da4e04e66d2c4509fca8bbc81ad2
Pinja Alanne <pinja.alanne@gmail.com>
2024-06-04 11:56:27 +0300

    KAN-52 Change bannerImage to bannerVideo

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,11 +1,11 @@
 import { Box } from '@mui/material'
-import banner from '../../assets/banner/banner.jpeg'
+import banner from '../../assets/banner/banner.mp4'
 import styles from './banner.module.css'
 
 const Banner = () => {
     return (
         <Box className={styles.bannerContainer}>
-            <img src={banner} alt="banner" className={styles.bannerImage} />
+            <video src={banner} autoPlay loop muted className={styles.bannerVideo} />
             <p className={styles.bannerText}>
                 Conoce nuestros productos y agrégalos a tu orden
             </p>
